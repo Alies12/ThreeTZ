d68d8f73fc8706ac92cf67dbb24d21a5a1400008
Alies12 <116558825+Alies12@users.noreply.github.com>
2025-03-17 10:46:29 +0300

    Revert "Switch to ts"

--- FirstTask/my-react-app/src/shared/components/Interface/popupStateInterface.tsx
+++
@@ -1,6 +0,0 @@
-import User from "./userInterface";
-export default interface PopupState {
-    isOpen: Boolean;
-    selectedUser: User | null;
-  }
-  
--- FirstTask/my-react-app/src/shared/components/Interface/userInterface.tsx
+++
@@ -1,9 +0,0 @@
-export default interface User {
-  name: string;
-  phone: string;
-  email: string;
-  address: string;
-  position_name: string;
-  department: string;
-  hire_date: string;
-}
